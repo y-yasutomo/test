0fa3c7bded20bd7b1aae04514c419066cd195257
y-yasutomo <52323198+y-yasutomo@users.noreply.github.com>
2020-12-07 20:09:23 +0900

    Add files via upload

--- gettime.R
+++ gettime.R
@@ -6,12 +6,6 @@ Hour<-strsplit(as.character(Time),":")[[1]][1]
 Min<-strsplit(as.character(Time),":")[[1]][2]
 Min<-as.numeric(Min)
 
-while(!(Min%in%c(0,15,30,45))){
-  Min<-Min-1 
-}
-
-stTime<-paste(Hour,Min,sep=":")
-if(Min==0)stTime<-paste0(stTime,"0")
 DateMat<-read.csv("C:/Users/sskai/Desktop/temp/format.csv")
 
 DateMat$Date<-as.character(DateMat$Date)
@@ -19,8 +13,18 @@ DateMat$sTime<-as.character(DateMat$sTime)
 DateMat$eTime<-as.character(DateMat$eTime)
 
 if(DateMat$eTime[nrow(DateMat)]==""){
+  while(!(Min%in%c(0,15,30,45))){
+    Min<-Min-1 
+  }
+  stTime<-paste(Hour,Min,sep=":")
+  if(Min==0)stTime<-paste0(stTime,"0")
   DateMat$eTime[length(DateMat$eTime)]<-as.character(stTime)
 }else{
+  while(!(Min%in%c(0,15,30,45))){
+    Min<-Min+1 
+  }
+  stTime<-paste(Hour,Min,sep=":")
+  if(Min==0)stTime<-paste0(stTime,"0")
   DateMat<-rbind(DateMat,c(as.character(as.Date(a)),as.character(stTime),""))
 }
 
