c59a719be868217f3df1da58bd7d96b0ee73c757
y-yasutomo <52323198+y-yasutomo@users.noreply.github.com>
2020-12-19 16:59:27 +0900

    Add files via upload

--- openxlsx.R
+++ openxlsx.R
@@ -11,21 +11,28 @@ addWorksheet(wb = NewWb, sheetName = "Sheet1")
 #範囲を指定し行,列の固定:firstActiveRowオプション,firstActiveColオプション
 freezePane(wb = NewWb, sheet = 1,
            firstRow = TRUE, firstCol= FALSE)
-writeData(wb = NewWb, sheet = 1, x = df)
-Sheet1Style <- createStyle(border = "TopBottomLeftRight",
-                           borderStyle = "thin",
-                           borderColour = "black")
+writeData(wb = NewWb, sheet = 1, x = df,borders = "all",
+          withFilter = T)
+if(F){
+  Sheet1Style <- createStyle(border = "TopBottomLeftRight",
+                             borderStyle = "thin",
+                             borderColour = "black") 
+  
+}
 
 SheetStyle_header <- createStyle(fgFill = "red",
                                  border = "TopBottomLeftRight",
                                  borderStyle = "thin",
-                                 borderColour = "black")
+                                 borderColour = "black",
+                                 textDecoration = "bold",
+                                 halign = "center",
+                                 valign = "center")
 
 SheetStyle_header2 <- createStyle(fgFill = "blue",
                                   border = "TopBottomLeftRight",
                                   borderStyle = "thin",
-                                  borderColour = "black")
-
+                                  borderColour = "black",
+                                  textDecoration = "bold")
 
 addStyle(wb = NewWb, sheet = 1, style = SheetStyle_header,
          rows = 1, cols = 1:2, gridExpand = T)
@@ -33,14 +40,18 @@ addStyle(wb = NewWb, sheet = 1, style = SheetStyle_header,
 addStyle(wb = NewWb, sheet = 1, style = SheetStyle_header2,
          rows = 1, cols = 3:ncol(df), gridExpand = T)
 
-addStyle(wb = NewWb, sheet = 1, style = Sheet1Style,
-         rows = 2:nrow(df), cols = 1:ncol(df), gridExpand = T)
+#addStyle(wb = NewWb, sheet = 1, style = Sheet1Style,
+#         rows = 2:nrow(df), cols = 1:ncol(df), gridExpand = T)
 
 
-addFilter(wb = NewWb, sheet = 1,rows = 1,cols = 1:ncol(df))
+#addFilter(wb = NewWb, sheet = 1,rows = 1,cols = 1:ncol(df))
 
 setColWidths(wb = NewWb, sheet = 1,
-             cols = 1:ncol(df), widths = "auto")
+             cols = 1:ncol(df), widths = 20)
+
+setRowHeights(wb = NewWb, sheet = 1,
+              rows = 1, heights  = 30)
+
 #showGridLines(wb = NewWb, sheet = 1, showGridLines = FALSE)
 saveWorkbook(wb = NewWb, file = "Test.xlsx", overwrite = TRUE)
 
